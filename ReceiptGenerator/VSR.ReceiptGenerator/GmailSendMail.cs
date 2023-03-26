f135cc1e2f82320c388c04a3b95dbc9de184b46b
Rajan Choudhary <Rajan.Choudhary@3Insys.com>
2023-03-26 23:14:43 +0530

    Adding Image to the Receipt

--- ReceiptGenerator/VSR.ReceiptGenerator/GmailSendMail.cs
+++ ReceiptGenerator/VSR.ReceiptGenerator/GmailSendMail.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net.Mail;
+using System.Net.Mime;
+using System.Text.RegularExpressions;
 
 namespace VSR.ReceiptGenerator
 {
@@ -27,12 +29,13 @@ namespace VSR.ReceiptGenerator
                     Body = emailBody,
                     From = from,
                     IsBodyHtml = true,
-                    BodyEncoding = System.Text.Encoding.Unicode,
+                    //BodyEncoding = System.Text.Encoding.Unicode,
                     HeadersEncoding = System.Text.Encoding.Unicode,
                     SubjectEncoding = System.Text.Encoding.Unicode,
                 };
                 mailmsg.To.Add(to);
-
+                AlternateView view = ContentToAlternateView(emailBody);
+                mailmsg.AlternateViews.Add(view);
                 smtp.Send(mailmsg);
             }
             catch (Exception ex)
@@ -40,6 +43,47 @@ namespace VSR.ReceiptGenerator
 
                 throw;
             }
+        }
+
+        private static AlternateView ContentToAlternateView(string content)
+        {
+            var imgCount = 0;
+            List<LinkedResource> resourceCollection = new List<LinkedResource>();
+            foreach (Match m in Regex.Matches(content, "<img(?<value>.*?)>"))
+            {
+                imgCount++;
+                 var imgContent = m.Groups["value"].Value;
+                string type = Regex.Match(imgContent, ":(?<type>.*?);base64,").Groups["type"].Value;
+                var base64 = Regex.Match(imgContent, "base64,(?<base64>.*?)\"");//.Groups["base64"].Value;
+                //Match base64 = Regex.Match(imgContent, "base64,(?<base64>.*?)\");
+                var groups1 = base64.Groups["base64"].Value;
+                if (String.IsNullOrEmpty(type) || String.IsNullOrEmpty(groups1))
+                {
+                    //ignore replacement when match normal <img> tag
+                    continue;
+                }
+                var replacement = " src=\"cid:" + imgCount + "\"";
+                content = content.Replace(imgContent, replacement);
+                var tempResource = new LinkedResource(Base64ToImageStream(groups1), new ContentType(type))
+                {
+                    ContentId = imgCount.ToString()
+                };
+                resourceCollection.Add(tempResource);
+            }
+
+            AlternateView alternateView = AlternateView.CreateAlternateViewFromString(content, null, MediaTypeNames.Text.Html);
+            foreach (var item in resourceCollection)
+            {
+                alternateView.LinkedResources.Add(item);
+            }
+
+            return alternateView;
+        }
+        public static Stream Base64ToImageStream(string base64String)
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
+            return ms;
         }
     }
 }
